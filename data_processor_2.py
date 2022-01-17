8f4886871b05acd02b25efeb7e1dcbb1b6355aab
Barzu Andrei-Daniel <andrewbarzu@gmail.com>
2022-01-17 12:18:52 +0200

    final commit

--- data_processor_2.py
+++ data_processor_2.py
@@ -7,7 +7,7 @@ scale = 3
 crop_size_lr = 48
 crop_size_hr = 48
 
-dataset = "dataset.txt"
+dataset = "dataset_cubic.txt"
 
 with open(dataset, "w") as f:
     pass
@@ -20,14 +20,14 @@ with open(dataset, "a") as f:
                     0:(image_decoded.shape[1] - (image_decoded.shape[1] % scale)), :]
         lr = cv2.resize(cropped, (int(cropped.shape[1] / scale), int(cropped.shape[0] / scale)),
                         interpolation=cv2.INTER_AREA)
-        upscaled = cv2.resize(lr, (lr.shape[1] * scale, lr.shape[0] * scale), interpolation=cv2.INTER_AREA)
+        upscaled = cv2.resize(lr, (lr.shape[1] * scale, lr.shape[0] * scale), interpolation=cv2.INTER_CUBIC)
 
         numx = int(lr.shape[0] / crop_size_lr)
         numy = int(lr.shape[1] / crop_size_lr)
         for i in range(0, numx, 4):
             startx = i * crop_size_lr
             endx = (i * crop_size_lr) + crop_size_lr
-            for j in range(0, numy, 4):
+            for j in range(0, numy, 2):
                 starty = j * crop_size_lr
                 endy = (j * crop_size_lr) + crop_size_lr
 
@@ -37,6 +37,6 @@ with open(dataset, "a") as f:
                 image_file_split = p.split('.')
                 image_name = image_file_split[0]
                 image_extension = image_file_split[1]
-                cv2.imwrite("./DIV2K_processed/" + image_name + "-" + str(i) + "-" + str(j) + "_X_" + "." + image_extension, crop_lr)
-                cv2.imwrite("./DIV2K_processed/" + image_name + "-" + str(i) + "-" + str(j) + "_y_" + "." + image_extension, crop_hr)
+                cv2.imwrite("./DIV2K_processed_cubic/" + image_name + "-" + str(i) + "-" + str(j) + "_X_" + "." + image_extension, crop_lr)
+                cv2.imwrite("./DIV2K_processed_cubic/" + image_name + "-" + str(i) + "-" + str(j) + "_y_" + "." + image_extension, crop_hr)
                 f.write(image_name + "-" + str(i) + "-" + str(j) + "\n")
